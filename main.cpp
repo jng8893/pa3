449f2e1e71d445da56d207807e35c9e0b14128e6
Jonathon Ng <jonn8893@gmail.com>
2018-04-05 01:26:04 -0400

    Can read file, now working on parsing text.

--- main.cpp
+++ main.cpp
@@ -1,26 +1,55 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-class Compiler
+/*class Compiler
 {
+    public:
+        vector<string> keywords();
+        vector<string> identifiers();
+        vector<string> constants();
+        vector<string> operators();
+        vector<string> delimiters();
 
-};
+        vector parseUppercase ();
+};*/
 
-int main() {
+int main()
+{
+    cout << "Please enter the name of the input file: " << endl;
+    string fileName;
+    cin >> fileName;
 
-    ifstream inFile("code.txt", ios::in);
-    if (! inFile)
+    ifstream inFile(fileName);
+    if (!inFile)
     {
         cerr << "Unable to open file.";
         return -1;
     }
+    inFile.open(fileName);
+
     char ch;
-    while (inFile.get(ch))
+    while (inFile.get(ch))  //Print out file contents.
     {
         cout.put(ch);
     }
+
+    vector<char> text;
+    while (inFile.get(ch))
+    {
+        text.push_back(ch);
+    }
+  /*  vector<char>::iterator it = text.begin();
+    for (auto it = text.begin(); it != text.end(); it++)
+    {
+        char temp;
+        temp = *it;
+        cout << temp;
+    }*/
+
 
     return 0;
 }
